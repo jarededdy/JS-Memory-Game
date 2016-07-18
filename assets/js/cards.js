527d851427058c9d26626a102c66a64e4a78ffbb
jaredgeddy <jaredgeddy@gmail.com>
2016-07-18 07:54:42 -0700

    added objects to cards.js and was able to access them from index.html

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -1,4 +1,20 @@
-var text = '{ "employees" : [' +
-'{ "firstName":"John" , "lastName":"Doe" },' +
-'{ "firstName":"Anna" , "lastName":"Smith" },' +
-'{ "firstName":"Peter" , "lastName":"Jones" } ]}';
+var cards = [
+	{ 
+		value: 10,
+		color: "#ff0000",
+		height:200,
+		width:100
+	},
+	{ 
+		value: 10,
+		color: "#00ff00",
+		height:200,
+		width:100
+	},
+	{ 
+		value: 10,
+		color: "#0000ff",
+		height:200,
+		width:100
+	}
+];
