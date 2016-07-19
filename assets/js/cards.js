78cc881999430249ac3d4590a6ca51e21bdd36aa
jaredgeddy <jaredgeddy@gmail.com>
2016-07-18 22:43:25 -0700

    created cards in a while loop, created a function to show the value of each card and reveal it's color

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -6,15 +6,28 @@ var cards = [
 		width:100
 	},
 	{ 
-		value: 10,
-		color: "#00ff00",
+		value: 20,
+		color: "#ff6600",
 		height:200,
 		width:100
 	},
 	{ 
-		value: 10,
+		value: 30,
 		color: "#0000ff",
 		height:200,
 		width:100
 	}
 ];
+
+function createCards() {
+	var i = 0;
+	while (cards[i]) {
+	  $('.gameboard').append("<a class=\"card number-" + i + "\" data-value=\"" + cards[i].value + "\" onClick=\"showValue("+i+")\";></a>");
+	  i++;
+	};
+};
+
+function showValue(i) {
+	alert( cards[i].value );
+	$('.number-'+i).css("background-color", cards[i].color);
+}
