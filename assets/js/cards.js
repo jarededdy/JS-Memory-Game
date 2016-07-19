0056145332a390f4341d5567d2462335383bf48c
jaredgeddy <jaredgeddy@gmail.com>
2016-07-18 22:45:39 -0700

    added comments to explain my work

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -20,14 +20,14 @@ var cards = [
 ];
 
 function createCards() {
-	var i = 0;
-	while (cards[i]) {
-	  $('.gameboard').append("<a class=\"card number-" + i + "\" data-value=\"" + cards[i].value + "\" onClick=\"showValue("+i+")\";></a>");
-	  i++;
+	var i = 0; //set to 0 always because thats the beginning of the array
+	while (cards[i]) { //loops through each array index
+	  $('.gameboard').append("<a class=\"card number-" + i + "\" data-value=\"" + cards[i].value + "\" onClick=\"showValue("+i+")\";></a>"); //loops through the cards array and creates HTML elements based on each object in the array
+	  i++; //continues loop
 	};
 };
 
 function showValue(i) {
-	alert( cards[i].value );
-	$('.number-'+i).css("background-color", cards[i].color);
+	alert( cards[i].value ); //reveals the value of the card
+	$('.number-'+i).css("background-color", cards[i].color); //reveals the color of the card
 }
