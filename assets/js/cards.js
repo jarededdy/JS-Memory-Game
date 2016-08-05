8f2d6cce215abb6e1b1dc4a786c7d05d9d6d058e
Jared Eddy <jaredgeddy@gmail.com>
2016-08-04 21:27:15 -0700

    added the functionality to clone and then shuffle the cards

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -13,15 +13,40 @@ var cards = [
 	}
 ];
 
+var cardsClone = cards.slice(); //clone the array cards
+var allCards = cards.concat(cardsClone); //combine the cloned array with the original cards array
+
+function shuffle(array) { //shuffle array that will shuffle the indexes of any array passed as an argument
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+shuffle(allCards); //shuffle the allCards array indexes
+
 function createCards() {
 	var i = 0; //set to 0 always because thats the beginning of the array
-	while (cards[i]) { //loops through each array index
-	  $('.gameboard').append("<a class=\"card number-" + i + "\" data-value=\"" + cards[i].value + "\" onClick=\"showValue("+i+")\";></a>"); //loops through the cards array and creates HTML elements based on each object in the array
+	while (allCards[i]) { //loops through each array index
+	  $('.gameboard').append("<a class=\"card number-" + i + "\" data-value=\"" + allCards[i].value + "\" onClick=\"showValue("+i+")\";></a>"); //loops through the cards array and creates HTML elements based on each object in the array
 	  i++; //continues loop
 	};
+	return allCards;
 };
 
 function showValue(i) {
-	alert( cards[i].value ); //reveals the value of the card
-	$('.number-'+i).css("background-color", cards[i].color); //reveals the color of the card
+	alert( allCards[i].value ); //reveals the value of the card
+	$('.number-'+i).css("background-color", allCards[i].color); //reveals the color of the card
 }
